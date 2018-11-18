02d8183ec1e75fe7fbc8b3dfad6df26ec7123ed3
zsiegel <zsiegel87@gmail.com>
2018-11-18 15:06:33 -0600

    small tweaks

--- linux.sh
+++ linux.sh
@@ -8,15 +8,11 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" > /etc/apt/
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 #Google Chrome
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+#echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/#sources.list.d/google-chrome.list
+#curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 
 #Kubernetes
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  > /etc/apt/sources.list.d/kubernetes.list
-
-#Dropbox
-echo "deb http://linux.dropbox.com/ubuntu xenial main"  > /etc/apt/sources.list.d/dropbox.list
-apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
 
 #OBS-Studio
 add-apt-repository ppa:obsproject/obs-studio
@@ -24,11 +20,9 @@ add-apt-repository ppa:obsproject/obs-studio
 apt update 
 apt upgrade -y
 
-apt install -y google-chrome-stable \
-		dconf-editor \
+apt install -y dconf-editor \
 		openjdk-8-jdk \
 		git \
-		albert \
 		clang \
 		bash-completion \
 		cmake \
@@ -41,7 +35,7 @@ apt install -y google-chrome-stable \
 		kubectl \
 		ffmpeg \
 		obs-studio \
-		dropbox \
+
 
 #Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
