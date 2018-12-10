0a60918fb980986af278cbf73c1e0a2f0abfe9a9
zsiegel <zsiegel87@gmail.com>
2018-12-10 17:55:39 -0600

    linux tweaks

--- linux.sh
+++ linux.sh
@@ -17,6 +17,10 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  > /etc/apt/sources.
 #OBS-Studio
 add-apt-repository ppa:obsproject/obs-studio
 
+#Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
 apt update 
 apt upgrade -y
 
@@ -35,13 +39,7 @@ apt install -y dconf-editor \
 		kubectl \
 		ffmpeg \
 		obs-studio \
-
-
-#Sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-apt install -y sublime-text
+		sublime-text
 
 apt autoremove
 apt autoclean
