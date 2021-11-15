094ae664334a46458ae6b5a9349231d9477b9cc2
zsiegel <304495+zsiegel@users.noreply.github.com>
2021-11-15 14:17:40 -0600

    mac script

---
+++ mac.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
