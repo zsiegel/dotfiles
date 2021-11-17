a03ec0f396f95c4f9b2b857703dd9b0e10cf94d5
zsiegel <304495+zsiegel@users.noreply.github.com>
2021-11-17 13:55:25 -0600

    install ZSH and switch!

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Install packages
+sudo apt-get install -y \
+    fonts-powerline \
+    zsh
+
+# Change Shell
+chsh -s /usr/bin/zsh $USERNAME
+
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
