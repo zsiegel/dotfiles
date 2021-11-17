2c697daf37fce6d96553a7dc47d9c8f3c0b01544
zsiegel <304495+zsiegel@users.noreply.github.com>
2021-11-17 13:39:58 -0600

    install script for github codespaces

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Move files
+cat .zshrc > $HOME/.zshrc
+cat .zprofile > $HOME/.zprofile
+cat .p10k.zsh > $HOME/.p10k.zsh
+
+cat .aliases > $HOME/.aliases
+cat .exports > $HOME/.exports
+cat .gitignore > $HOME/.gitignore
+cat .gitconfig > $HOME/.gitconfig
