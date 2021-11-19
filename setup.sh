7f7ffeb320169ccf3c267bbbba0fdb3a81ac4b74
zsiegel <304495+zsiegel@users.noreply.github.com>
2021-11-18 20:37:13 -0600

    remove sudo. re-arrange stuff.

--- setup.sh
+++ setup.sh
@@ -7,20 +7,21 @@ case "$(uname -s)" in
     ;;
     Linux)
         echo "Installing linux packages"
-        sudo apt-get install -y \
+        apt-get install -y \
+            curl \
             fonts-powerline \
             zsh        
     ;;
 esac
 
-# Change Shell
-chsh -s /usr/bin/zsh $USERNAME
-
 # Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Change Shell
+chsh -s $(which zsh) $USERNAME
 
 # Move files
 cat .zshrc > $HOME/.zshrc
