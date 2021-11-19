fd2385487b96f11f186ee4475f76b838ecd15235
zsiegel <304495+zsiegel@users.noreply.github.com>
2021-11-18 22:32:24 -0600

    add back change shell

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Change Shell
-# chsh -s $(which zsh) $USERNAME
+chsh -s $(which zsh) $USERNAME
 
 # Move files
 cat .zshrc > $HOME/.zshrc
