40e0d56318f58a8f359ab1fc1ab125ba482fcf3b
zsiegel <1332734-zsiegel@users.noreply.gitlab.com>
2021-11-24 11:47:21 -0600

    update JDK installation with brew

--- brew.sh
+++ brew.sh
@@ -8,7 +8,11 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-brew install grep git wget curl fastlane JakeWharton/repo/dockerfile-shebang pyenv openjdk@11
+
+brew install grep git wget curl fastlane JakeWharton/repo/dockerfile-shebang pyenv
+
+brew tap mdogan/zulu
+brew install --cask zulu-jdk17 zulu-jdk16 zulu-jdk11 zulu-jdk8
 
 # Remove outdated versions from the cellar.
 brew cleanup
